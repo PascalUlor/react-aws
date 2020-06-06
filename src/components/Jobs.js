0f711bc75a9f1e428cdfab459614ec500f6bc86b
PascalUlor <pascalulor@yahoo.com>
2020-06-06 19:30:03 +0100

    edit jbs comp

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,15 +1,20 @@
 import { API } from 'aws-amplify';
 import React, { useState, useEffect } from 'react';
 
-let apiName = 'awsrest'
-let path = '/api/'
+const apiName = 'awsrest';
+const path = '/api/';
+const myInit = { // OPTIONAL
+    headers: {}, // OPTIONAL
+    response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+};
+
 
 
 export default function Jobs() {
     const [jobs, setJobs] = useState();
 
     useEffect( () => {
-        API.get(apiName, path, { response: true})
+        API.get(apiName, path, myInit)
         .then((result) =>{
             console.log(result.data)
             setJobs(result.data);
