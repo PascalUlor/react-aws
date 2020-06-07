89fcdb26cbdfba15ba0c2eac8af94279ed558dc7
PascalUlor <pascalulor@yahoo.com>
2020-06-07 23:34:20 +0100

    render modal for each job post

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import Amplify, { API, Storage} from "aws-amplify";
+import React from "react";
+import Amplify, { API, Storage } from "aws-amplify";
 import awsconfig from "./aws-exports";
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import './App.css';
-import Jobs from './components/Jobs';
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "./App.css";
+import Jobs from "./components/Jobs";
+import Header from "./components/Header";
 
 Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
-      <Jobs/>
+    <div>
+     <Header />
+      <div className="App">
+        <Jobs />
+      </div>
     </div>
   );
 }
 
 // export default App;
 
-
-const authTheme = {navBar: {backgroundColor: '556cd6', border: 0, color: 0}}
+const authTheme = {
+  navBar: { backgroundColor: "556cd6", border: 0, color: 0 },
+};
 export default withAuthenticator(App, {
-includeGreetings: true, theme: authTheme
+  includeGreetings: true,
+  theme: authTheme,
 });
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,8 +1,11 @@
 import { API } from 'aws-amplify';
 import React, { useState, useEffect } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
-import { makeStyles, styled } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
 import AvgSalary from './AvgSalary'
+import JobReviewCard from './JobReviewCard'
+
 
 const apiName = 'awsrest';
 const path = '/api/';
@@ -11,20 +14,65 @@ const myInit = { // OPTIONAL
     response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
 };
 
+function rand() {
+    return Math.round(Math.random() * 20) - 10;
+  }
+  
+  function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  
+  const cardStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 
 const useStyles = makeStyles({
     table: {
         minWidth: 700,
+        margin: '50px 0 0 0',
     },
     div: {
         padding: 10,
-    }
+    },
+    thead: {
+        fontWeight: 'bold',
+        fontSize: '1.5em',
+    },
 });
 
 
 export default function Jobs() {
     const [jobs, setJobs] = useState();
     const classes = useStyles();
+    const modalClass = cardStyles();
+
+    // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+  const [card, setCard] = useState({});
+
+  const handleOpen = (data) => {
+    setOpen(true);
+    setCard(data)
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
     useEffect( () => {
         API.get(apiName, path, myInit)
@@ -40,17 +88,17 @@ export default function Jobs() {
                 <Table className={classes.table} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Company</TableCell>
-                            <TableCell align="right">Job Title</TableCell>
-                            <TableCell align="right">Type</TableCell>
-                            <TableCell align="right">Created at</TableCell>
-                            <TableCell align="right">Location</TableCell>
-                            <TableCell align="right">Average Salary</TableCell>
+                            <TableCell className={classes.thead}>Company</TableCell>
+                            <TableCell className={classes.thead} align="right">Job Title</TableCell>
+                            <TableCell className={classes.thead} align="right">Type</TableCell>
+                            <TableCell className={classes.thead} align="right">Created at</TableCell>
+                            <TableCell className={classes.thead} align="right">Location</TableCell>
+                            <TableCell className={classes.thead} align="right">Average Salary</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {jobs.map(row =>(
-                            <TableRow key={row.id}>
+                            <TableRow key={row.id} onClick={()=>handleOpen(row)}>
                                 <TableCell align="right">{row.company}</TableCell>
                                 <TableCell align="right">{row.title}</TableCell>
                             <TableCell align="right">{row.type}</TableCell>
@@ -62,6 +110,14 @@ export default function Jobs() {
                     </TableBody>
                 </Table>
             </TableContainer>
+            <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <JobReviewCard modalStyle={modalStyle} modalClass={modalClass} card={card} />
+      </Modal>
         </main>
     ) : (
         <div className={classes.div}>Loading...</div>
